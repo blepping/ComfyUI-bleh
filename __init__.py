50b0ac7665fced4b5a959caa5f8f48d1fbc5736b
blepping <157360029+blepping@users.noreply.github.com>
2024-04-11 12:22:22 -0600

    Add BlehBlockOps, BlehLatentOps nodes (#3)
    Add experimental nodes to allow performing arbitrary model patches and latent operations.
    
    Refactoring.

--- __init__.py
+++ __init__.py
@@ -5,21 +5,25 @@ settings.load_settings()
 if settings.SETTINGS.btp_enabled:
     from .py import betterTaesdPreview  # noqa: F401
 
-from .py import (
-    deepshrink,
-    hypertile,
+from .py.nodes import (
+    deepShrink,
+    hyperTile,
     modelPatchConditional,
+    ops,
     refinerAfter,
     samplers,
     sigmas,
 )
 
 NODE_CLASS_MAPPINGS = {
-    "BlehHyperTile": hypertile.HyperTileBleh,
-    "BlehDeepShrink": deepshrink.DeepShrinkBleh,
+    "BlehBlockOps": ops.BlehBlockOps,
+    "BlehDeepShrink": deepShrink.DeepShrinkBleh,
     "BlehDiscardPenultimateSigma": sigmas.DiscardPenultimateSigma,
-    "BlehInsaneChainSampler": samplers.BlehInsaneChainSampler,
     "BlehForceSeedSampler": samplers.BlehForceSeedSampler,
+    "BlehHyperTile": hyperTile.HyperTileBleh,
+    "BlehInsaneChainSampler": samplers.BlehInsaneChainSampler,
+    "BlehLatentScaleBy": ops.BlehLatentScaleBy,
+    "BlehLatentOps": ops.BlehLatentOps,
     "BlehModelPatchConditional": modelPatchConditional.ModelPatchConditionalNode,
     "BlehRefinerAfter": refinerAfter.BlehRefinerAfter,
 }
